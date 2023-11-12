ef061bb95f783976b4f47aae4472799c1591c3e4
Flame-eth <theflamepen@gmail.com>
2023-11-13 00:26:01 +0100

    car price estimation

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,16 +6,29 @@ const CustomButton = ({
   title,
   containerStyles,
   handleClick,
-  btnType
+  btnType,
+  isDisabled,
+  rightIcon,
+  textStyles,
 }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
-      type={ btnType ?? "button"}
+      disabled={isDisabled}
+      type={btnType ?? "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
