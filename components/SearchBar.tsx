40c614643a971a96bf7aa9f61d1388a1ba640d55
Flame-eth <theflamepen@gmail.com>
2023-11-13 14:59:30 +0100

    search bar ui mod

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,44 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import { SearchManufacturer } from ".";
+import Image from "next/image";
+
+const SearchButton: React.FC<{
+  otherClasses?: string;
+}> = ({ otherClasses }) => (
+  <button
+    type="submit"
+    className={` -ml-3 z-10 ${otherClasses}`}
+    title="Search"
+  >
+    <Image
+      src="/magnifying-glass.svg"
+      width={40}
+      height={40}
+      className="object-contain"
+      alt="Search"
+    />
+  </button>
+);
 
 const SearchBar: React.FC = () => {
-
-    const [manufacturer, setManufacturer] = useState<string>('')
+  const [manufacturer, setManufacturer] = useState<string>("");
   const handleSearch = () => {};
-  return <form action="" className="searchbar" onSubmit={handleSearch}>
-    <div className="searchbar__item">
-
-        <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
-    </div>
-  </form>;
+  return (
+    <form action="" className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <div className="searchbar__item">
+        <Image src="/model-icon.png" width={25} height={25} className="absolute w-[20px] h-[20px] ml-4" alt="car model" />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
--- utils/index.ts
+++ utils/index.ts
@@ -9,7 +9,7 @@ export async function fetchCars() {
 
   try {
     const response = await axios.get(
-      "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera&limit=12",
+      "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3&limit=12",
       { headers }
     );
     return response.data;
@@ -40,22 +40,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return totalPrice.toFixed(0);
 };
 
-
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL(`https://cdn.imagin.studio/getimage`);
 
   const { make, model, year } = car;
 
-  url.searchParams.append("customer", process.env.NEXT_PUBLIC_IMAGIN_KEY ?? '');
+  url.searchParams.append("customer", process.env.NEXT_PUBLIC_IMAGIN_KEY ?? "");
 
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
-  url.searchParams.append("zoomType", 'fullscreen');
+  url.searchParams.append("zoomType", "fullscreen");
   url.searchParams.append("modelYear", `${year}`);
-  url.searchParams.append("angle", `${angle ?? 'front'}`);
+  url.searchParams.append("angle", `${angle ?? "front"}`);
 
   return url.toString();
-
-
-
- }
+};
