5c07d593c0e4fa634bf70167e10bfba3027cd8cd
Flame-eth <theflamepen@gmail.com>
2023-11-14 10:12:34 +0100

    custom filter ui done

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 
 export default async function Home({
@@ -34,8 +35,8 @@ export default async function Home({
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter />
-            <CustomFilter />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client";
+import {useState} from "react";
+import { CustomFilterProps } from "@/types";
+import { Listbox } from "@headlessui/react";
+import Image from "next/image";
 
-const CustomFilter: React.FC = () => {
+const CustomFilter: React.FC<CustomFilterProps> = ({
+  title, options
+}) => {
+  const [selected, setSelected] = useState(options[0]);
   return (
-    <div>CustomFilter</div>
+    <div className="w-fit ">
+      <Listbox value={selected} onChange={(e) => setSelected(e)} >
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">
+              {selected.title}
+            </span>
+            <Image src="/chevron-up-down.svg" width={20} height={20} alt="chevron-down" className="ml-4 object-contain" />
+
+          </Listbox.Button>
+        </div>
+
+      </Listbox>
+
+    </div>
   )
 }
 
