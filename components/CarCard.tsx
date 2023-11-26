5bb01567486f326c7bcd194359587d20c64db092
Flame-eth <theflamepen@gmail.com>
2023-11-27 00:15:00 +0100

    ui fix

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -30,15 +30,15 @@ const CarCard: React.FC<CarCardProps> = ({ car }) => {
   return (
     <AnimatePresence>
       {!isAnimating && (
-       <motion.div
-      //  className="car-card"
-       initial={{ scale: 1, opacity: 1 }}
-       animate={{ scale: isOpen ? 0 : 1, opacity: isOpen ? 0 : 1 }}
-       exit={{ scale: 2, opacity: 0 }}
-       transition={{ duration: 0.5 }} // adjust this duration to your liking
-     >
-          <motion.div
-            className="car-card group"
+        <motion.div
+          className="car-card group"
+          initial={{ scale: 1, opacity: 1 }}
+          animate={{ scale: isOpen ? 0 : 1, opacity: isOpen ? 0 : 1 }}
+          exit={{ scale: 2, opacity: 0 }}
+          transition={{ duration: 0.5 }} // adjust this duration to your liking
+        >
+          {/* <motion.div
+            className=""
             initial={{
               opacity: 0,
               scale: 0.2,
@@ -54,81 +54,81 @@ const CarCard: React.FC<CarCardProps> = ({ car }) => {
               duration: 0.3,
               type: "spring",
             }}
+          > */}
+          <div className="car-card__content">
+            <h2 className="car-card__content-title">
+              {make} {model}
+            </h2>
+          </div>
+
+          <p className="flex mt-6 text-[32px] font-extrabold">
+            <span className="self-start text-[14px] font-semibold ">$</span>
+            {carRent}
+            <span className="self-end text-[14px] font-semibold">/day</span>
+          </p>
+
+          <motion.div
+            className="relative w-full h-40 my-3 object-contain "
+            initial={{
+              x: "-100%",
+            }}
+            whileInView={{
+              x: 0,
+            }}
+            transition={{
+              duration: 1,
+            }}
           >
-            <div className="car-card__content">
-              <h2 className="car-card__content-title">
-                {make} {model}
-              </h2>
-            </div>
-
-            <p className="flex mt-6 text-[32px] font-extrabold">
-              <span className="self-start text-[14px] font-semibold ">$</span>
-              {carRent}
-              <span className="self-end text-[14px] font-semibold">/day</span>
-            </p>
-
-            <motion.div
-              className="relative w-full h-40 my-3 object-contain "
-              initial={{
-                x: "-100%",
-              }}
-              whileInView={{
-                x: 0,
-              }}
-              transition={{
-                duration: 1,
-              }}
-            >
-              <Image
-                src={generateCarImageUrl(car)}
-                alt="car model"
-                fill
-                priority
-                className="object-contain"
-              />
-            </motion.div>
-
-            <div className="relative flex w-full mt-2">
-              <div className="flex group-hover:invisible w-full justify-between text-gray">
-                <div className="flex flex-col justify-center items-center gap-2">
-                  <Image
-                    src="/steering-wheel.svg"
-                    width={20}
-                    height={20}
-                    alt="steering wheel"
-                  />
-                  <p className="text-[14px]">
-                    {transmission === "a" ? "Automatic" : "Manual"}
-                  </p>
-                </div>
-                <div className="flex flex-col justify-center items-center gap-2">
-                  <Image src="/tire.svg" width={20} height={20} alt="tire" />
-                  <p className="text-[14px]">{drive.toUpperCase()}</p>
-                </div>
-                <div className="flex flex-col justify-center items-center gap-2">
-                  <Image src="/gas.svg" width={20} height={20} alt="gas" />
-                  <p className="text-[14px]">{city_mpg} MPG</p>
-                </div>
-              </div>
-
-              <div className="car-card__btn-container">
-                <CustomButton
-                  title="View More"
-                  containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
-                  textStyles="text-white text-[14px] font-bold leading-[17px]
-            "
-                  rightIcon="/right-arrow.svg"
-                  handleClick={() => handleClick()}
-                />
-              </div>
-            </div>
-
-            <CarDetails
-              isOpen={isOpen}
-              closeModal={() => setIsOpen(false)}
-              car={car}
+            <Image
+              src={generateCarImageUrl(car)}
+              alt="car model"
+              fill
+              priority
+              className="object-contain"
             />
           </motion.div>
+
+          <div className="relative flex w-full mt-2">
+            <div className="flex group-hover:invisible w-full justify-between text-gray">
+              <div className="flex flex-col justify-center items-center gap-2">
+                <Image
+                  src="/steering-wheel.svg"
+                  width={20}
+                  height={20}
+                  alt="steering wheel"
+                />
+                <p className="text-[14px]">
+                  {transmission === "a" ? "Automatic" : "Manual"}
+                </p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-2">
+                <Image src="/tire.svg" width={20} height={20} alt="tire" />
+                <p className="text-[14px]">{drive.toUpperCase()}</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-2">
+                <Image src="/gas.svg" width={20} height={20} alt="gas" />
+                <p className="text-[14px]">{city_mpg} MPG</p>
+              </div>
+            </div>
+
+            <div className="car-card__btn-container">
+              <CustomButton
+                title="View More"
+                containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+                textStyles="text-white text-[14px] font-bold leading-[17px]
+            "
+                rightIcon="/right-arrow.svg"
+                handleClick={() => handleClick()}
+              />
+            </div>
+          </div>
+
+          <CarDetails
+            isOpen={isOpen}
+            closeModal={() => setIsOpen(false)}
+            car={car}
+          />
+          {/* </motion.div> */}
         </motion.div>
       )}
     </AnimatePresence>
